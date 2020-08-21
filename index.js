8dd841c47b8e567c418990031d73bbf7e4438be1
Doug Cash <dcash@Dougs-MacBook-Pro.local>
2020-08-21 12:09:07 -0500

    EOC 9 - Refactor for MVC

--- index.js
+++ index.js
@@ -1,67 +1,31 @@
 const express = require('express')
-const path = require('path')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const BlogPost = require('./models/BlogPost.js')
 const fileUpload = require('express-fileupload')
-
-const validateMiddleWare = (req,res,next)=>{
-    if(req.files == null || req.body.title == null || req.body.title == null){
-        return res.redirect('/posts/new')
-    }
-    next()
-}
-
+const newPostController = require('./controllers/newPost')
+const homeController = require('./controllers/home')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+const validateMiddleware = require('./middleware/validationMiddleware')
 
 const app = new express()
+
 app.set('view engine', 'ejs')
+
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
 app.use(fileUpload())
-app.use('/posts/store', validateMiddleWare)
+
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
 
 app.listen(4000, ()=>{
     console.log("App listening on port 4000")
 })
 
-app.get('/', async (req,res)=>{
-    const blogposts = await BlogPost.find({})
-    res.render('index', {
-        blogposts
-    });
-})
-
-app.get('/about', (req,res)=>{
-    res.render('about');
-
-})
-
-app.get('/contact', (req,res)=>{
-    res.render('contact');
-
-})
-
-app.get('/post/:id', async (req,res)=>{
-    const blogpost = await BlogPost.findById(req.params.id)
-    res.render('post',{
-        blogpost
-    });
-})
-
-app.get('/posts/new', (req,res)=>{
-    res.render('create');
-})
-
-app.post('/posts/store', async (req,res)=>{
-    let image = req.files.image;
-    image.mv(path.resolve(__dirname, 'public/img', image.name), async (error)=>{
-        await BlogPost.create({
-            ...req.body,
-            image: '/img/' + image.name
-        })
-        res.redirect('/')
-    })
-})
+app.get('/', homeController)
+app.get('/posts/new', newPostController)
+app.use('/posts/store', validateMiddleware)
+app.get('/post/:id', getPostController)
+app.post('/posts/store', storePostController)
